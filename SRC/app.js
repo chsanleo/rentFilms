bc87fb937ac7ab03e59f4a5f3f3ed23a9666d454
Christian <chsanleo@gmail.com>
2020-06-18 12:41:06 +0200

    Create Cors Middleware and import

--- SRC/app.js
+++ SRC/app.js
@@ -7,7 +7,10 @@ const usersRouter = require('./routers/users');
 const moviesRouter = require('./routers/movies');
 const ordersRouter = require('./routers/orders');
 
+const cors = require('./middleware/cors');
+
 app.use(express.json());
+app.use(cors);
 
 app.use('/main', mainRouter);
 app.use('/users', usersRouter);
