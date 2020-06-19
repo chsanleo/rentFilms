a641d9a28cfaec856c1b8961b164585d291b3444
Christian <chsanleo@gmail.com>
2020-06-19 15:18:00 +0200

    Fix migration Token

--- SRC/migrations/20200618110901-create-token.js
+++ SRC/migrations/20200618110901-create-token.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Token', {
+    return queryInterface.createTable('Tokens', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Token');
+    return queryInterface.dropTable('Tokens');
   }
 };
