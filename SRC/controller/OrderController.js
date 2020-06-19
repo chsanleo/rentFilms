55351dcf00bd57ac59016b7f3c03e4b4ff7f0ee4
Christian <chsanleo@gmail.com>
2020-06-19 17:44:15 +0200

    getOrder by Id

--- SRC/controller/OrderController.js
+++ SRC/controller/OrderController.js
@@ -1,25 +1,36 @@
 const { Order } = require('../models');
 
 const OrderController = {
-    async addOrder (req, res) {
+    async addOrder(req, res) {
         try {
             await Order.create(req.body);
-            res.status(201).send({message:"Created order success."});
-            
+            res.status(201).send({ message: "Created order success." });
+
         } catch (error) {
             res.status(500).send({ message: "There was a problem" });
         }
     },
 
-    async getOrder (req, res) {
+    async getOrder(req, res) {
+        try {
+            let order = await Order.findOne({
+                 where: { 
+                     id: req.params.id
+                    },
+            });
+            res.status(200).send(order);
+
+        } catch (error) { 
+            res.status(500).send({ message: "There was a problem" });
+
+        }
+    },
+
+    async updateOrder(req, res) {
 
     },
 
-    async updateOrder (req, res) {
-
-    },
-
-    async deleteOrder (req, res) {
+    async deleteOrder(req, res) {
 
     }
 }
