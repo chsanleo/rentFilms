56218407ad9494eb10f570d87aa4ab8e68dedd61
Christian <chsanleo@gmail.com>
2020-06-21 16:52:35 +0200

    Update properties sample with mongoDB data

--- SRC/config/properties-sample.js
+++ SRC/config/properties-sample.js
@@ -1,9 +1,12 @@
 const PROPERTIES={
     token_SECRETWORD : 'sample',
-    externAPI_KEY : 'sampleKey',
+    externAPI_KEY : 'sample',
     externAPI_LANGUAGE : 'es-ES',
     LIMITMOVIESRENT : 1,
-    PASSWORDSALT : 1
+    PASSWORDSALT : 1,
+    MONGO_USER:'sample',
+    MONGO_PASS:'sample',
+    MONGO_NAMEDB:'sample'
 }
 
 module.exports = PROPERTIES;
