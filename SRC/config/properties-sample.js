4e3fadae9cd4d163e21f3d7329940088ac6d9fe8
Christian <chsanleo@gmail.com>
2020-06-21 02:36:53 +0200

    Update properties-sample

--- SRC/config/properties-sample.js
+++ SRC/config/properties-sample.js
@@ -1,1 +1,8 @@
-const SECRETWORD = 'sample';
+const PROPERTIES={
+    token_SECRETWORD : 'sample',
+    externAPI_KEY : 'sampleKey',
+    externAPI_LANGUAGE : 'es-ES',
+    LIMITMOVIESRENT : 1
+}
+
+module.exports = PROPERTIES;
