5da0921332acff92f836dd7104057227f30fe9fb
Christian <chsanleo@gmail.com>
2020-06-21 21:25:05 +0200

    Correct file with properties

--- SRC/config/mongoDb.js
+++ SRC/config/mongoDb.js
@@ -1,7 +1,8 @@
+const properties = require('./properties');
 const dbconnect = () => {
 
     const mongoose = require("mongoose");
-    const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0-20sv3.mongodb.net/${MONGO_NAMEDB}?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${properties.MONGO_USER}:${properties.MONGO_PASS}@cluster0-20sv3.mongodb.net/${properties.MONGO_NAMEDB}?retryWrites=true&w=majority`;
     mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -9,8 +10,6 @@ const dbconnect = () => {
         }).then(() => {
             console.log('Connection established.');
         })
-        .catch(error => console.log('Error connecting:' + error));
-
-}
+        .catch(error => console.log('Error connecting:' + error));}
 
 module.exports = dbconnect;
