c7dedbcf4ea8c944b4fe265bc5efa1efe59a3554
Christian <chsanleo@gmail.com>
2020-06-21 21:26:42 +0200

    Delete add endpoint, no useful

--- SRC/routers/movies.js
+++ SRC/routers/movies.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const MovieController = require('../controller/MovieController');
 
-router.post('/', MovieController.addMovie);
 router.get('/:id', MovieController.getMovie);
 router.get('/genders/:id', MovieController.getGenders);
 router.get('/trending/:id', MovieController.getTredingMovies)
