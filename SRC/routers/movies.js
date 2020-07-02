27dddd8d5f190ab44d0ccf65aa71837847c4d673
Christian <chsanleo@gmail.com>
2020-07-02 18:03:29 +0200

    Fix error in routing

--- SRC/routers/movies.js
+++ SRC/routers/movies.js
@@ -4,6 +4,6 @@ const MovieController = require('../controller/MovieController');
 router.get('/:id', MovieController.getMovie);
 router.get('/genders/:id', MovieController.getGenders);
 router.get('/trending/:id', MovieController.getTredingMovies);
-router.post('title', MovieController.getMoviesByTitle);
-
+router.post('/title', MovieController.getMoviesByTitle);
+router.post('/movies', MovieController.getSomeMovies);
 module.exports = router;
