3dc642ae612ac07bfac0861f3c8ea7f15ef960fd
Christian <chsanleo@gmail.com>
2020-07-05 23:00:06 +0200

    Fix add order
    Cause, auth from FRONT doesnt work

--- SRC/controller/OrderController.js
+++ SRC/controller/OrderController.js
@@ -5,16 +5,17 @@ const MovieController = require('../controller/MovieController');
 const OrderController = {
     async addOrder(req, res) {
         try {
-            let rented = Order.findAndCountAll({
-                where : {
-                    UserId : req.User.id,
-                    returnDate : null
+            let rented = await Order.findAndCountAll({
+                where: {
+                    UserId: req.body.userID,
+                    returnDate: null
                 }
             });
-            if(rented.count > properties.LIMITMOVIESRENT){
-                res.status(500).send({message : "Already rented the limit of films"});
+            console.log("despues find all")
+            if (rented.count > properties.LIMITMOVIESRENT) {
+                res.status(500).send({ message: "Already rented the limit of films" });
             }
-
+            console.log("intentamos Crear")
             await Order.create(req.body);
             res.status(201).send({ message: "Created order success." });
 
@@ -26,13 +27,13 @@ const OrderController = {
     async getOrder(req, res) {
         try {
             let order = await Order.findOne({
-                 where: { 
-                     id: req.params.id
-                    },
+                where: {
+                    id: req.params.id
+                },
             });
             res.status(200).send(order);
 
-        } catch (error) { 
+        } catch (error) {
             res.status(500).send({ message: "There was a problem" });
 
         }
@@ -40,18 +41,18 @@ const OrderController = {
 
     async getAllOrder(req, res) {
         try {
-            let orders = await Order.findAll({ });
+            let orders = await Order.findAll({});
             res.status(200).send(orders);
 
-        } catch (error) { 
+        } catch (error) {
             res.status(500).send({ message: "There was a problem" });
 
         }
     },
 
-    async getAllOrdersByUser(req,res){
+    async getAllOrdersByUser(req, res) {
         try {
-            let orders = await Order.findAll({ 
+            let orders = await Order.findAll({
                 where: {
                     UserId: req.user.id,
                 }
@@ -59,14 +60,14 @@ const OrderController = {
 
             let ordersReturn;
             let title;
-            
-            for(let order of orders){
+
+            for (let order of orders) {
                 title = MovieController.getMovieById(order.MovieId);
-                ordersReturn.push( new OrderFilm (order.id, order.MovieId, title,order.createAt, order.returnDate));
+                ordersReturn.push(new OrderFilm(order.id, order.MovieId, title, order.createAt, order.returnDate));
             }
 
             res.status(200).send(ordersReturn);
-            
+
         } catch (error) {
             res.status(500).send({ message: "There was a problem" });
 
@@ -75,12 +76,12 @@ const OrderController = {
 
     async updateOrder(req, res) {
         try {
-            await Order.update(req.body,{
-                where:{
-                    id:req.body.id
+            await Order.update(req.body, {
+                where: {
+                    id: req.body.id
                 }
             });
-            res.send({message:`Updated correctly Order ${req.body.id}`});
+            res.send({ message: `Updated correctly Order ${req.body.id}` });
         } catch (error) {
             res.status(500).send({ message: "There was a problem" });
 
@@ -90,11 +91,11 @@ const OrderController = {
     async deleteOrder(req, res) {
         try {
             await Order.destroy({
-                where:{
-                    id:req.params.id
+                where: {
+                    id: req.params.id
                 },
             })
-            res.send({message:`Deleted successfull Order ${req.body.id}`})
+            res.send({ message: `Deleted successfull Order ${req.body.id}` })
         } catch (error) {
             res.status(500).send({ message: "There was a problem" });
         }
