61aa3109bbaaa456ec0144cf56bcc84ce6556720
Christian <chsanleo@gmail.com>
2020-07-06 01:30:32 +0200

    configurate to use without token

--- SRC/routers/orders.js
+++ SRC/routers/orders.js
@@ -4,7 +4,7 @@ const OrderController = require('../controller/OrderController');
 router.post('/', OrderController.addOrder);
 router.get('/:id', OrderController.getOrder);
 router.get('/', OrderController.getAllOrder);
-router.get('/user',OrderController.getAllOrdersByUser);
+router.get('/user/:id',OrderController.getAllOrdersByUser);
 router.put('/', OrderController.updateOrder);
 router.delete('/:id', OrderController.deleteOrder);
 
